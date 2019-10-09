42128547bc322b39eeb163eaf1d7e1862bd29f5c
Lucilla <lucilla@pinterest.com>
2019-10-09 10:33:08 -0700

    Adding Pinterest specific conf and teletraan files
    Summary: upgrading druid to 0.16.0
    
    Reviewers: #druideng
    
    Subscribers: mleonard, #ads-reporting, #security
    
    Differential Revision: https://phabricator.pinadmin.com/D462815

---
+++ examples/bin/start_docker.sh
@@ -0,0 +1,118 @@
+#! /bin/bash
+
+sed -i "s/<DRUID_CLUSTER_NAME>/${TELETRAAN_DRUID_CLUSTER_NAME}/g" /opt/druid/conf/druid/_common/common.runtime.properties
+sed -i "s/<DRUID_ZK_SERVICE_HOST>/${TELETRAAN_DRUID_ZK_SERVICE_HOST}/g" /opt/druid/conf/druid/_common/common.runtime.properties
+
+export KNOX_MACHINE_AUTH=$(hostname)
+mysql_creds=$(knox get mysql:rbac:longqueryrw:credentials)
+mysql_user=$(cut -d'@' -f1 <<< ${mysql_creds})
+
+sed -i "s/<MYSQL_USER>/${mysql_user}/" /opt/druid/conf/druid/_common/common.runtime.properties
+
+nohup /opt/druid/bin/monitor_mysql_master.sh /var/druid/metadata.uri /var/config/config.services.general_mysql_databases_config >> /var/log/druid/mysql_monitor.log 2>&1 &
+
+DRUID_LIB_DIR=/opt/druid/lib
+DRUID_CONF_DIR=/opt/druid/conf/druid
+DRUID_LOG_DIR=/var/log/druid
+
+node='unset'
+
+case ${STAGE_NAME} in
+  *"master"* | *"coordinator"*)
+    node=coordinator
+    sed -i "s/<MEM_MIN>/${TELETRAAN_DRUID_MEM:-30}/" $DRUID_CONF_DIR/coordinator/jvm.config
+    sed -i "s/<MEM_MAX>/${TELETRAAN_DRUID_MEM:-30}/" $DRUID_CONF_DIR/coordinator/jvm.config
+    sed -i "s/<COORDINATOR_PORT>/${TELETRAAN_COORDINATOR_PORT:-9090}/" $DRUID_CONF_DIR/coordinator/runtime.properties
+
+    if [ ! -z "${TELETRAAN_DRUID_TIER_MIRRORING_MAP}" ]; then
+      if grep -q "druid.coordinator.tier.tierMirroringMap" $DRUID_CONF_DIR/coordinator/runtime.properties; then
+        sed -i "s/^druid.coordinator.tier.tierMirroringMap=.*$/druid.coordinator.tier.tierMirroringMap=${TELETRAAN_DRUID_TIER_MIRRORING_MAP}/" $DRUID_CONF_DIR/coordinator/runtime.properties
+      else
+        sed -i "\$adruid.coordinator.tier.tierMirroringMap=${TELETRAAN_DRUID_TIER_MIRRORING_MAP}" $DRUID_CONF_DIR/coordinator/runtime.properties
+       fi
+    fi
+
+  ;;
+  *"overlord"*)
+    node=overlord
+    sed -i "s/<OVERLORD_PORT>/${TELETRAAN_OVERLORD_PORT:-9090}/" $DRUID_CONF_DIR/overlord/runtime.properties
+  ;;
+  *"router"*)
+    node=router
+  ;;
+  *"query"*)
+    sed -i "s/<HTTP_CLIENT_CONNECTIONS>/${TELETRAAN_HTTP_CLIENT_CONNECTIONS:-300}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<HTTP_SERVER_THREADS>/${TELETRAAN_HTTP_SERVER_THREADS:-60}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<HTTP_MAX_QUEUED_BYTES>/${TELETRAAN_HTTP_MAX_QUEUED_BYTES:-14680064}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<DRUID_BROKER_HTTP_UNUSED_CONNECTION_TIMEOUT>/${TELETRAAN_DRUID_BROKER_HTTP_UNUSED_CONNECTION_TIMEOUT:-PT50S}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<DRUID_BROKER_HTTP_READ_TIMEOUT>/${TELETRAAN_DRUID_BROKER_HTTP_READ_TIMEOUT:-PT15M}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<DRUID_SERVER_HTTP_MAX_IDLE_TIME>/${TELETRAAN_DRUID_SERVER_HTTP_MAX_IDLE_TIME:-PT5M}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<THREADS>/${TELETRAAN_DRUID_THREADS:-7}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<PROCESSING_BUFFER_SIZEBYTES>/${TELETRAAN_PROCESSING_BUFFER_SIZEBYTES:-1073741824}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<PROCESSING_NUM_MERGE_BUFFER>/${TELETRAAN_PROCESSING_NUM_MERGE_BUFFER:-4}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<GROUP_BY_INTERMEDIATE_COMBINE_DEGREE>/${TELETRAAN_GROUP_BY_INTERMEDIATE_COMBINE_DEGREE:-8}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<GROUP_BY_PARALLEL_COMBINE_THREADS>/${TELETRAAN_GROUP_BY_PARALLEL_COMBINE_THREADS:-16}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<DRUID_BROKER_BALANCER_TYPE>/${TELETRAAN_DRUID_BROKER_BALANCER_TYPE:-connectionCount}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<USE_APPROXIMATE_COUNT_DISTINCT>/${TELETRAAN_USE_APPROXIMATE_COUNT_DISTINCT:-true}/" $DRUID_CONF_DIR/broker/runtime.properties
+    sed -i "s/<USE_APPROXIMATE_TOPN>/${TELETRAAN_USE_APPROXIMATE_TOPN:-true}/" $DRUID_CONF_DIR/broker/runtime.properties
+
+    if [ ! -z "${TELETRAAN_DATASOURCE_COMPLEMENT_MAP}" ]; then
+      if grep -q "druid.broker.dataSourceComplement.mapping" $DRUID_CONF_DIR/broker/runtime.properties; then
+        sed -i "s/^druid.broker.dataSourceComplement.mapping=.*$/druid.broker.dataSourceComplement.mapping=${TELETRAAN_DATASOURCE_COMPLEMENT_MAP}/" $DRUID_CONF_DIR/broker/runtime.properties
+      else
+        sed -i "\$adruid.broker.dataSourceComplement.mapping=${TELETRAAN_DATASOURCE_COMPLEMENT_MAP}" $DRUID_CONF_DIR/broker/runtime.properties
+      fi
+    fi
+
+    sed -i "s/<MEM_MIN>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/broker/jvm.config
+    sed -i "s/<MEM_MAX>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/broker/jvm.config
+    sed -i "s/<NEW_SIZE>/${TELETRAAN_DRUID_NEW_SIZE:-4}/" $DRUID_CONF_DIR/broker/jvm.config
+    sed -i "s/<MAX_DIRECT_MEM>/${TELETRAAN_DRUID_MAX_DIRECT_MEM:-12}/" $DRUID_CONF_DIR/broker/jvm.config
+
+    node=broker
+  ;;
+  *"data"*)
+    # Data tier differences are managed through Teletraan script configs
+    sed -i "s/<HTTP_SERVER_THREADS>/${TELETRAAN_HTTP_SERVER_THREADS:-60}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<THREADS>/${TELETRAAN_DRUID_THREADS:-7}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<PROCESSING_BUFFER_SIZEBYTES>/${TELETRAAN_PROCESSING_BUFFER_SIZEBYTES:-1073741824}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<PROCESSING_NUM_MERGE_BUFFER>/${TELETRAAN_PROCESSING_NUM_MERGE_BUFFER:-4}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<GROUP_BY_INTERMEDIATE_COMBINE_DEGREE>/${TELETRAAN_GROUP_BY_INTERMEDIATE_COMBINE_DEGREE:-8}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<GROUP_BY_PARALLEL_COMBINE_THREADS>/${TELETRAAN_GROUP_BY_PARALLEL_COMBINE_THREADS:-16}/" $DRUID_CONF_DIR/historical/runtime.properties
+
+    sed -i "s/<TIER>/${TELETRAAN_DRUID_TIER:-_default_tier}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<MAX_SIZE>/${TELETRAAN_DRUID_MAX_SIZE:-1000000000000}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<LOCATIONS_MAX_SIZE>/${TELETRAAN_DRUID_LOCATIONS_MAX_SIZE:-1000000000000}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_HISTORICAL_CACHE_USE_CACHE>/${TELETRAAN_DRUID_HISTORICAL_CACHE_USE_CACHE:-true}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_HISTORICAL_CACHE_POPULATE_CACHE>/${TELETRAAN_DRUID_HISTORICAL_CACHE_POPULATE_CACHE:-true}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_HISTORICAL_CACHE_SIZE>/${TELETRAAN_DRUID_HISTORICAL_CACHE_SIZE:-2147483647}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_SERVER_HTTP_MAX_IDLE_TIME>/${TELETRAAN_DRUID_SERVER_HTTP_MAX_IDLE_TIME:-PT5M}/g" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_NUM_THREADS_TO_LOAD_SEGMENTS_INTO_PAGE_CACHE_ON_DOWNLOAD>/${TELETRAAN_DRUID_NUM_THREADS_TO_LOAD_SEGMENTS_INTO_PAGE_CACHE_ON_DOWNLOAD:-0}/" $DRUID_CONF_DIR/historical/runtime.properties
+    sed -i "s/<DRUID_NUM_THREADS_TO_LOAD_SEGMENTS_INTO_PAGE_CACHE_ON_BOOTSTRAP>/${TELETRAAN_DRUID_NUM_THREADS_TO_LOAD_SEGMENTS_INTO_PAGE_CACHE_ON_BOOTSTRAP:-0}/" $DRUID_CONF_DIR/historical/runtime.properties
+
+    sed -i "s/<MEM_MIN>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/historical/jvm.config
+    sed -i "s/<MEM_MAX>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/historical/jvm.config
+    sed -i "s/<NEW_SIZE>/${TELETRAAN_DRUID_NEW_SIZE:-4}/" $DRUID_CONF_DIR/historical/jvm.config
+    sed -i "s/<MAX_DIRECT_MEM>/${TELETRAAN_DRUID_MAX_DIRECT_MEM:-12}/" $DRUID_CONF_DIR/historical/jvm.config
+
+    node=historical
+  ;;
+  *"middleManager"*)
+    node=middleManager
+    sed -i "s/<MIDDLE_MANAGER_PORT>/${TELETRAAN_MIDDLE_MANAGER_PORT:-8091}/" $DRUID_CONF_DIR/middleManager/runtime.properties
+    sed -i "s/<DRUID_INDEXER_RUNNER_START_PORT>/${TELETRAAN_DRUID_INDEXER_RUNNER_START_PORT:-8100}/" $DRUID_CONF_DIR/middleManager/runtime.properties
+    sed -i "s/<DRUID_INDEXER_RUNNER_END_PORT>/${TELETRAAN_DRUID_INDEXER_RUNNER_END_PORT:-65535}/" $DRUID_CONF_DIR/middleManager/runtime.properties
+    sed -i "s/<THREADS>/${TELETRAAN_DRUID_THREADS:-7}/" $DRUID_CONF_DIR/middleManager/runtime.properties
+    sed -i "s/<PROCESSING_BUFFER_SIZEBYTES>/${TELETRAAN_PROCESSING_BUFFER_SIZEBYTES:-1073741824}/" $DRUID_CONF_DIR/middleManager/runtime.properties
+
+    sed -i "s/<MEM_MIN>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/middleManager/jvm.config
+    sed -i "s/<MEM_MAX>/${TELETRAAN_DRUID_MEM:-8}/" $DRUID_CONF_DIR/middleManager/jvm.config
+    sed -i "s/<NEW_SIZE>/${TELETRAAN_DRUID_NEW_SIZE:-4}/" $DRUID_CONF_DIR/middleManager/jvm.config
+  ;;
+  *)
+    echo "Unknown stage ${STAGE_NAME}! Unable to automatically start any Druid nodes!"
+    exit 1
+  ;;
+esac
+
+exec java `cat $DRUID_CONF_DIR/$node/jvm.config | xargs` -cp $DRUID_CONF_DIR/_common:$DRUID_CONF_DIR/$node:$DRUID_LIB_DIR/* org.apache.druid.cli.Main server $node >> $DRUID_LOG_DIR/$node.log 2>&1
