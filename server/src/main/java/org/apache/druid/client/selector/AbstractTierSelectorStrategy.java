cc44851e7dae711dfef3ac6324c660c45f7b10ff
ericnguyen <ericnguyen@pinterest.com>
2020-10-11 12:39:45 -0700

    Support routing to historical based on query's priority
    Summary:
    This allows to configure the routing based on the priority of the request,
    Example: historical 1 with priority of 1 and historical 2 with priority of 2, configuration is 5->2, 3->1
    then queries with priority of 5 will be routed to historical 2, queries with priority of 3 will be routed to historical 1.  Queries with priorities that are not in the map will default to highest tier selector strategy, similarly, if the historical priority does not exist
    No changes in behavior if not configured to use queryPriorityBased
    
    Reviewers: O1139 Druid, lucilla
    
    Reviewed By: O1139 Druid, lucilla
    
    Subscribers: mleonard
    
    Differential Revision: https://phabricator.pinadmin.com/D631184
    
    (cherry picked from commit bdd1782f1c773378219f2789c23ee95d98741611)

--- server/src/main/java/org/apache/druid/client/selector/AbstractTierSelectorStrategy.java
+++ server/src/main/java/org/apache/druid/client/selector/AbstractTierSelectorStrategy.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ *
  */
 public abstract class AbstractTierSelectorStrategy implements TierSelectorStrategy
 {
@@ -51,6 +52,18 @@ public abstract class AbstractTierSelectorStrategy implements TierSelectorStrate
     return Iterables.getOnlyElement(pick(query, prioritizedServers, segment, 1), null);
   }
 
+  @Nullable
+  @Override
+  public QueryableDruidServer pick(
+      int queryPriority,
+      Int2ObjectRBTreeMap<Set<QueryableDruidServer>> prioritizedServers,
+      DataSegment segment
+  )
+  {
+    // If not configured to use the priority based tier selector strategy, just ignore the priority input
+    return Iterables.getOnlyElement(pick(prioritizedServers, segment, 1), null);
+  }
+
   @Override
   public <T> List<QueryableDruidServer> pick(
       Query<T> query,
@@ -61,11 +74,20 @@ public abstract class AbstractTierSelectorStrategy implements TierSelectorStrate
   {
     List<QueryableDruidServer> result = new ArrayList<>(numServersToPick);
     for (Set<QueryableDruidServer> priorityServers : prioritizedServers.values()) {
-      result.addAll(serverSelectorStrategy.pick(query, priorityServers, segment, numServersToPick - result.size()));
+      result.addAll(pick(priorityServers, segment, numServersToPick - result.size()));
       if (result.size() == numServersToPick) {
         break;
       }
     }
     return result;
+  }
+
+  protected List<QueryableDruidServer> pick(
+      Set<QueryableDruidServer> priorityServers,
+      DataSegment segment,
+      int numServersToPick
+  )
+  {
+    return serverSelectorStrategy.pick(priorityServers, segment, numServersToPick);
   }
 }
