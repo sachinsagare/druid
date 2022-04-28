3ba28c1c047090eef3ac86c25d043737138ad994
ssagare <ssagare@pinterest.com>
2022-04-27 21:31:47 -0400

    Only use onsite namespace for lifetime table
    Summary: When query hit lifetime table, only use onsite namespace
    
    Reviewers: O1139 Druid, ericnguyen
    
    Reviewed By: O1139 Druid, ericnguyen
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.pinadmin.com/D641122

--- core/src/main/java/org/apache/druid/timeline/ComplementaryNamespacedVersionedIntervalTimeline.java
+++ core/src/main/java/org/apache/druid/timeline/ComplementaryNamespacedVersionedIntervalTimeline.java
@@ -103,6 +103,19 @@ public class ComplementaryNamespacedVersionedIntervalTimeline<VersionType, Objec
       // should be covered by this base timeline
       NamespacedVersionedIntervalTimeline<VersionType, ObjectType> baseTimeline =
               supportTimelinesByDataSource.get(supportTimelinesByDataSource.lastKey());
+      Map<String, List<Interval>> namespaceToRemainingInterval;
+
+      if (isLifetime) {
+        namespaceToRemainingInterval = supportTimelinesByDataSource.get(dataSource).getNamespaces().stream()
+                .map(ComplementaryNamespacedVersionedIntervalTimeline::getRootNamespace)
+                .distinct()
+                .collect(Collectors.toMap(namespace -> namespace, namespace -> intervals));
+      } else {
+        namespaceToRemainingInterval = baseTimeline.getNamespaces().stream()
+                .map(ComplementaryNamespacedVersionedIntervalTimeline::getRootNamespace)
+                .distinct()
+                .collect(Collectors.toMap(namespace -> namespace, namespace -> intervals));
+      }
 
       Map<String, List<Interval>> namespaceToRemainingInterval = baseTimeline.getNamespaces().stream()
               .map(ComplementaryNamespacedVersionedIntervalTimeline::getRootNamespace)
